610f51a2deaf76e2ad291d7cef42b3cb8b3db99e
Michal Konrad Owsiak <michalo@pi.local>
2017-05-03 20:43:16 +0200

    First draft of adapter pattern

---
+++ recipeNo028/java/Main.java
@@ -0,0 +1,10 @@
+import recipeNo028.*;
+
+public class Main extends recipeNo028.Main {
+
+	public static int fun()	{
+	  return recipeNo028.Main.fun();
+        }
+
+}
+
---
+++ recipeNo028/java/recipeNo028/Main.java
@@ -0,0 +1,13 @@
+package recipeNo028;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello, world");
+    }
+
+    public static int fun() {
+        System.out.println("From JVM - inside package");
+        return 0;
+    }
+}
