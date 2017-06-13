8aee880aa348b743537c3e7b82093303690ff150
Michal Konrad Owsiak <michalo@pi.local>
2017-06-13 15:33:12 +0200

    updated with comments

--- recipeNo025/c/Simple.cpp
+++ recipeNo025/c/Simple.cpp
@@ -2,17 +2,21 @@
 #include <string>
 #include "extern.h"
 
+/* Supper simple C++ class. We want to call it from Java */
+
 class Simple {
-public:
-  Simple(int val);
-  void showVal();
-  ~Simple();
-private:
-  int val;
+  public:
+    Simple(int val);
+    void showVal();
+    ~Simple();
+  private:
+    int val;
 };
 
 Simple::Simple(int val): val(val) {}
- 
+
+/* All we want to do, is to show value that we have passed
+   from Java to C, and then, to C++ */ 
 void Simple::showVal() {
   std::cout << "You have passed: " << val << "!" << std::endl;
 }
@@ -21,11 +25,13 @@ Simple::~Simple() {
   std::cout << "done!" << std::endl;
 }
 
+/* This code will be called from C. It's marked as extern "C" */
 void testSimple() {
   Simple two( 2 );
   two.showVal();
 }
 
+/* If you want to test it from CLI, go ahead */
 int main()
 {
   Simple one( 1 );
--- recipeNo025/c/recipeNo025_HelloWorld.cpp
+++ recipeNo025/c/recipeNo025_HelloWorld.cpp
@@ -3,6 +3,8 @@
 #include "recipeNo025_HelloWorld.h"
 #include "extern.h"
 
+/* All we do here is letting developer know that we are running
+   and call C++ code via testSimple() */
 JNIEXPORT void JNICALL Java_recipeNo025_HelloWorld_displayMessage
   (JNIEnv *env, jclass obj) {
 
