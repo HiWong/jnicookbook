61cc4b44427b6277d5984111b3f2156e55b86d77
Michal Konrad Owsiak <michalo@pi.local>
2017-06-17 21:14:49 +0200

    Added sample code that shows how to use native code from JAR

---
+++ recipeNo031/java/recipeNo031/HelloWorld.java
@@ -0,0 +1,28 @@
+/* HelloWorld.java */
+package recipeNo031;
+
+public class HelloWorld {
+
+  // Location of native code will be passed from the
+  // outside. In this case, Main class makes sure
+  // to extract native code from JAR file
+  String libFileLocation = null;
+
+  /* This is the native method we want to call */
+  public native void displayMessage();
+
+  // In constructor we pass only location of the lib
+  // file
+  public HelloWorld(String lib) {
+    this.libFileLocation = lib;
+  }
+
+  // This method will load native code
+  // and call native method declared above
+  public void callNativeMethod() {
+    System.load(libFileLocation);
+    displayMessage(); 
+  }
+
+}
+
---
+++ recipeNo031/java/recipeNo031/Main.java
@@ -0,0 +1,69 @@
+package recipeNo031;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+/*
+  In this sample I will create jar file with dynamic library.
+  This library will be extracted from a file stored inside JAR.
+  Due to the fact we want to be cross system compatible we are
+  creating ".lib" file instead of dylib (macOS) or so (Linux).
+*/
+public class Main {
+
+  /*
+    We are extracting library file and provide location of this
+    file inside temporary location
+  */
+  private String extractLibrary() {
+    try {
+      // Create temporary file
+      File file = File.createTempFile("libHelloWorld", ".lib");
+       
+      // In case it worked, we can extract lib
+      if (file.exists()) {
+        // First of all, let's show where do we plan to extract it
+        System.out.println("Temporary file: " + file.getAbsoluteFile().toPath());
+
+        // Now, we can get the lib file from JAR and put it inside temporary location
+        InputStream link = (getClass().getResourceAsStream("/lib/libHelloWorld.lib"));
+
+        // We want to overwrite existing file. This is why we are using
+        //
+        // java.nio.file.StandardCopyOption.REPLACE_EXISTING
+        Files.copy(link, file.getAbsoluteFile().toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+        return file.getAbsoluteFile().toPath().toString();
+      }
+      // In case something goes wrong, we will simply return null
+      return null;
+
+    } catch(IOException e) {
+      // The same goes for exception - we are passing null back
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+
+  /*
+    This is main method that
+    - extracts library from JAR
+    - calls native method declared inside HelloWorld class
+  */
+  public static void main(String [] arg) {
+
+    // First of all, we need to extrac library file 
+    Main main = new Main();
+    String libLocation = main.extractLibrary();
+ 
+    // Once it is extracted, we can call native code
+    // We are passing library location as argument
+    // for HelloWorld constructor
+    if(libLocation != null) {
+      HelloWorld helloWorld = new HelloWorld(libLocation);
+      helloWorld.callNativeMethod();
+    }
+  }
+}
