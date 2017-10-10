cce9fcab4a5d2ce43872a43ed119b5d0c2e1b3c6
mko <michal@owsiak.org>
2017-10-10 20:25:56 +0200

    Added sample for passing 2D arrays into JNI

---
+++ recipeNo039/c/recipeNo039_PassArray2D.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "jni.h"
+#include "recipeNo039_PassArray2D.h"
+
+JNIEXPORT int JNICALL Java_recipeNo039_PassArray2D_displayArray2D
+  (JNIEnv *env, jclass obj) {
+	
+  /* Get objarg's class - objarg is the one we pass from
+     Java */
+  jclass cls = (*env)->GetObjectClass(env, obj); 
+
+  /* Remember that you can alway get method signature using javap tool
+     > javap -classpath target -s -p recipeNo039.PassArray2D | grep -A +1 getDoubleData
+         public double[][] getDoubleData();
+           descriptor: ()[[D
+  */
+  
+  jmethodID mid = 
+    (*env)->GetMethodID(env, cls, "getDoubleData", "()[[D"); 
+
+  /* We have to make sure that method exists */
+  if (mid == NULL) {
+    return -1; /* method not found */ 
+  } 
+
+  /* Now, it's time for getting array of arrays */
+  jobject objArray = (*env)->CallObjectMethod(env, obj, mid);
+
+  /* We will iterate over the array so we can get into next array */
+  int arraySize = (*env)->GetArrayLength(env, objArray);
+
+  /* We want to go over all elements (all sub arrays) */
+  for (int i=0; i < arraySize; i++) 
+  {
+    jdoubleArray array = (*env)->GetObjectArrayElement(env, objArray, i);
+    
+    /* get size of the array */
+    jsize len = (*env)->GetArrayLength(env, array);
+ 
+    /* get the body of array; it will be referecende by C pointer */
+    jdouble *body = (*env)->GetDoubleArrayElements(env, array, 0);
+ 
+    /* do some stuff */
+    for(int p=0; p < len; p++) {
+      printf("Double value [%d;%d]: %f\n", i, p, body[p]);
+    }
+ 
+    /* release body when you decide it is no longer needed */
+    (*env)->ReleaseDoubleArrayElements(env, array, body, 0);
+  }
+
+  return 0;
+
+}
---
+++ recipeNo039/java/recipeNo039/PassArray2D.java
@@ -0,0 +1,31 @@
+/* PassArray2D.java */
+package recipeNo039;
+
+public class PassArray2D {
+
+  /* This is the native method we want to call */
+  public native int displayArray2D();
+
+  /* Inside static block we will load shared library */
+  static {
+    System.loadLibrary("PassArray2D");
+  }
+ 
+  private double [][] doubleData = { {0,1}, {2,3}, {4,5} };
+
+  public double[][] getDoubleData() {
+    return doubleData;
+  }
+
+  public static void main(String[] args) {
+    /* This message will help you determine whether
+       LD_LIBRARY_PATH is correctly set
+    */
+    System.out.println("library: " 
+      + System.getProperty("java.library.path"));
+ 
+    PassArray2D obj = new PassArray2D();
+    obj.displayArray2D();
+  }
+}
+
