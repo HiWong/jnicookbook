9d24e35c83cd5d792f29dab5f753de21200fc1de
Michal Owsiak <michal@owsiak.org>
2019-04-03 21:13:20 +0200

    Small fixes and formatting.

--- recipeNo005/c/recipeNo005_PassShort.c
+++ recipeNo005/c/recipeNo005_PassShort.c
@@ -3,8 +3,8 @@
 #include "recipeNo005_PassShort.h"
 
 JNIEXPORT void JNICALL Java_recipeNo005_PassShort_displayShort
-  (JNIEnv *env, jclass obj, jshort value) {
+                (JNIEnv * env, jclass obj, jshort value) {
 
-	printf("Passed value: %hd\n", value);
+  printf("Passed value: %hd\n", value);
 
 }
--- recipeNo005/java/recipeNo005/PassShort.java
+++ recipeNo005/java/recipeNo005/PassShort.java
@@ -3,25 +3,23 @@ package recipeNo005;
 
 public class PassShort {
 
-	/* This is the native method we want to call */
-	public static native void displayShort(short value);
+  /* This is the native method we want to call */
+  public static native void displayShort(short value);
 
-	/* Inside static block we will load shared library */
+  /* Inside static block we will load shared library */
   static {
     System.loadLibrary("PassShort");
   }
 
   public static void main(String[] args) {
-		/* This message will help you determine whether
-				LD_LIBRARY_PATH is correctly set
-		*/
-    System.out.println("library: " 
-			+ System.getProperty("java.library.path"));
-		
-		short value = 1234;
+    /* This message will help you determine whether
+    		LD_LIBRARY_PATH is correctly set
+    */
+    System.out.println("library: " + System.getProperty("java.library.path"));
 
-		/* Call to shared library */
+    short value = 1234;
+
+    /* Call to shared library */
     PassShort.displayShort(value);
   }
 }
-
