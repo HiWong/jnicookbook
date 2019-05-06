0a0f0ee5b8e87ce9c02e60853a1a3bf63341ef34
Michal Owsiak <michal@owsiak.org>
2019-05-06 20:08:18 +0200

    Added new recipe for Thread based JNI calls

---
+++ recipeNo044/README.md
@@ -0,0 +1,6 @@
+# getting Thread's id and name from JNI via java.lang.Thread
+
+This sample code shows how to retrieve current thread and it's name and id from JNI
+
+    make 
+    make test
---
+++ recipeNo044/java/recipeNo044/GetCurrentThread.java
@@ -0,0 +1,27 @@
+/* GetCurrentThread sample */
+package recipeNo044;
+
+public class GetCurrentThread {
+
+  /* This is the native method we want to call */
+  public static native void displayMessage();
+
+  /* Inside static block we will load shared library */
+  static {
+    System.loadLibrary("GetCurrentThread");
+  }
+
+  public static void main(String[] args) {
+    /* This message will help you determine whether
+    		LD_LIBRARY_PATH is correctly set
+    */
+    System.out.println("library: " + System.getProperty("java.library.path"));
+
+    /* Call to shared library */
+    System.out.println(   "[Java] name: " 
+                        + Thread.currentThread().getName() 
+                        + " id: " 
+                        + Thread.currentThread().getId() );
+    GetCurrentThread.displayMessage();
+  }
+}
