2dcebb0c504d04b37a029608d9cf761b095a308e
Michal Owsiak <michal@owsiak.org>
2019-05-08 19:13:53 +0200

    And yet another formatting ... uff

--- recipeNo024/c/recipeNo024_HelloWorld.c
+++ recipeNo024/c/recipeNo024_HelloWorld.c
@@ -3,36 +3,39 @@
 #include "jni.h"
 #include "recipeNo024_HelloWorld.h"
 
-void mode_1() {
-	printf("Mode 1\n");
+void mode_1 () {
+  printf ("Mode 1\n");
 }
 
-void mode_2() {
-	printf("Mode 2\n");
+void mode_2 () {
+  printf ("Mode 2\n");
 }
 
-void mode_3() {
-	printf("Mode 3\n");
+void mode_3 () {
+  printf ("Mode 3\n");
 }
 
-void mode_default() {
-	printf("Mode default\n");
+void mode_default () {
+  printf ("Mode default\n");
 }
 
 JNIEXPORT void JNICALL Java_recipeNo024_HelloWorld_displayMessage
-  (JNIEnv *env, jclass obj, int mode) {
-	int i=0;
-	while(i++<3) {
-		sleep(10);
-		switch (mode) {
-			case 1: mode_1();
-				break;
-			case 2: mode_2();
-				break;
-			case 3: mode_3();
-				break;
-			default:
-				mode_default();
-		}
-	}
+  (JNIEnv * env, jclass obj, int mode) {
+  int i = 0;
+  while (i++ < 3) {
+    sleep (10);
+    switch (mode) {
+      case 1:
+        mode_1 ();
+        break;
+      case 2:
+        mode_2 ();
+        break;
+      case 3:
+        mode_3 ();
+        break;
+      default:
+        mode_default ();
+    }
+  }
 }
--- recipeNo024/java/recipeNo024/HelloWorld.java
+++ recipeNo024/java/recipeNo024/HelloWorld.java
@@ -3,38 +3,35 @@ package recipeNo024;
 
 public class HelloWorld {
 
-	/* This is the native method we want to call */
-	public static native void displayMessage(int mode);
+  /* This is the native method we want to call */
+  public static native void displayMessage(int mode);
 
-	/* Inside static block we will load shared library */
+  /* Inside static block we will load shared library */
   static {
     System.loadLibrary("HelloWorld");
   }
 
   public static void main(String[] args) {
-		/* This message will help you determine whether
-				LD_LIBRARY_PATH is correctly set
-		*/
-    System.out.println("library: " 
-			+ System.getProperty("java.library.path"));
+    /* This message will help you determine whether
+    		LD_LIBRARY_PATH is correctly set
+    */
+    System.out.println("library: " + System.getProperty("java.library.path"));
 
-		/* Call to shared library */
-	new Thread() {
-    		public void run() {
-        	  HelloWorld.displayMessage(1);
-    		}
-	}.start();
-	new Thread() {
-    		public void run() {
-        	  HelloWorld.displayMessage(2);
-    		}
-	}.start();
-	new Thread() {
-    		public void run() {
-        	  HelloWorld.displayMessage(3);
-    		}
-	}.start();
-
+    /* Call to shared library */
+    new Thread() {
+      public void run() {
+        HelloWorld.displayMessage(1);
+      }
+    }.start();
+    new Thread() {
+      public void run() {
+        HelloWorld.displayMessage(2);
+      }
+    }.start();
+    new Thread() {
+      public void run() {
+        HelloWorld.displayMessage(3);
+      }
+    }.start();
   }
 }
-
