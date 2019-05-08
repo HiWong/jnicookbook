01c937f38aafc707218a37af119630f45b4c11d1
Michal Owsiak <michal@owsiak.org>
2019-05-08 19:11:43 +0200

    Further formatting

--- recipeNo023/c/recipeNo023_HelloWorld.c
+++ recipeNo023/c/recipeNo023_HelloWorld.c
@@ -2,13 +2,13 @@
 #include "jni.h"
 #include "recipeNo023_HelloWorld.h"
 
-void anotherFunction();
+void anotherFunction ();
 
 JNIEXPORT void JNICALL Java_recipeNo023_HelloWorld_displayMessage
-  (JNIEnv *env, jclass obj) {
+  (JNIEnv * env, jclass obj) {
 
-	printf("Hello world!\n");
-	/* We are calling function from another source */
+  printf ("Hello world!\n");
+  /* We are calling function from another source */
 
-	anotherFunction();
+  anotherFunction ();
 }
